c668b9a81f8fd2148d6da300c4e29450fef3f0b2
Caio <caiorodriguesmll@gmail.com>
2023-07-26 15:33:56 -0400

    Adding new endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
 from apps.core.views import ListViewSet, ItemViewSet
+from rest_framework.authtoken import views
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -12,5 +13,6 @@ router.register(r'item', ItemViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
     path('', include(router.urls)),
 ]
