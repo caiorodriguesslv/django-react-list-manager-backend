8cda65b30b6c64933df234e7064e4cada05eebac
Caio <caiorodriguesmll@gmail.com>
2023-07-25 20:45:30 -0400

    Create Apps and endpoint listing

--- backend/urls.py
+++ backend/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
+from apps.core.views import ListViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'list', ListViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
